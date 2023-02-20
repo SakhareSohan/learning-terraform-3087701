813be4b9b630c8c854b3f5853e063f9c98a6632d
Sohan Sakhare <101473969+SakhareSohan@users.noreply.github.com>
2023-02-20 18:29:14 +0530

    second

--- main.tf
+++ main.tf
@@ -14,11 +14,45 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+resource ""
+
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.blog_sg.security_group_ids]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "blog"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = module.vpc_public_subnets[0]
+
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = [all-all]
+  rgress_cidr_block = ["0.0.0.0/0"]
 }
